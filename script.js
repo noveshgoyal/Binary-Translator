88f122313231c04d7c9ebaf92c1e13c3d10f6f92
noveshgoyal <goyalnovesh@gmail.com>
2022-09-19 09:32:56 +0530

    feat- add function for string to binary conversion

---
+++ script.js
@@ -0,0 +1,35 @@
+const container = document.querySelector('.container');
+const input = document.createElement('input');
+const btnTranslate = document.createElement('button');
+const outputLabel = document.createElement('div');
+const output = document.createElement('div');
+const binary = "https://api.funtranslations.com/translate/binary.json"; // api for text to binary trasnlation
+
+const elements = [input, btnTranslate, outputLabel, output]
+
+function displayElements() {
+    elements.map((ele) => {
+        container.appendChild(ele);
+    })
+    btnTranslate.innerText = "Translate";
+    outputLabel.innerText = "Translated text: "
+}
+
+function errorHandler(error) {
+    alert("There is an error in the server try after sometime! ")
+}
+
+function textToBinary() {
+    displayElements();
+    input.placeholder = "Enter your text";
+    btnTranslate.addEventListener('click', () => {
+        let url = `${binary}?text=${input.value}`; // url making
+        fetch(url)
+            .then(response => response.json())
+            .then(json => {
+                let translatedText = json.contents.translated;
+                output.innerText = translatedText;
+            })
+            .catch(errorHandler);
+    })
+}
