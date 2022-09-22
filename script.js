b34718eb3807e09945e6dbdd6aaeda493c266e0f
noveshgoyal <goyalnovesh@gmail.com>
2022-09-22 08:36:17 +0530

    fix- a bug fix for the translate button

--- script.js
+++ script.js
@@ -1,48 +1,64 @@
-const container = document.querySelector('.container');
-const input = document.createElement('input');
-const btnTranslate = document.createElement('button');
-const outputLabel = document.createElement('div');
-const output = document.createElement('div');
-const binary = "https://api.funtranslations.com/translate/binary.json"; // api for text to binary trasnlation
+const container = document.querySelector(".container");
+const input = document.getElementById("input");
+const translateBtn = document.getElementById("translate-btn");
+const outputLabel = document.getElementById("output-label");
+const output = document.getElementById("output");
+const toTextBtn = document.getElementById("to-string-btn");
+const toBinaryBtn = document.getElementById("to-binary-btn");
+const binary = "https://api.funtranslations.com/translate/binary.json"; // api for string to binary trasnlation
 
-const elements = [input, btnTranslate, outputLabel, output]
-
-function displayElements() {
-    elements.map((ele) => {
-        container.appendChild(ele);
-    })
-    btnTranslate.innerText = "Translate";
-    outputLabel.innerText = "Translated text: "
-}
-
-function errorHandler(error) {
-    alert("There is an error in the server try after sometime! ")
+function errorHandler() {
+  alert("There is an error in the server try after sometime! ");
 }
 
 function textToBinary() {
-    displayElements();
-    input.placeholder = "Enter your text";
-    btnTranslate.addEventListener('click', () => {
-        let url = `${binary}?text=${input.value}`; // url making
-        fetch(url)
-            .then(response => response.json())
-            .then(json => {
-                let translatedText = json.contents.translated;
-                output.innerText = translatedText;
-            })
-            .catch(errorHandler);
-    })
+  if (input.value === " ") {
+    // check for an empty input
+    alert("Please enter a valid input");
+  } else {
+    let url = `${binary}?text=${input.value}`; // url making
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        let translatedText = json.contents.translated;
+        output.innerText = translatedText;
+      })
+      .catch(errorHandler);
+  }
 }
 
 function binaryToText() {
-    displayElements();
-    input.placeholder = "Enter your binary number";
-    btnTranslate.addEventListener('click', () => {
-        let translatedText = input.value
-        .split(' ')
-        .map(b => parseInt(b, 2))
-        .map(num => String.fromCharCode(num)).join('');
-        console.log(translatedText);
-        output.innerText = translatedText;
-    })
+  if (input.value === " ") {
+    // check for an empty input
+    alert("Please enter a valid input");
+  } else {
+    let translatedText = input.value
+      .split(" ")
+      .map((b) => parseInt(b, 2))
+      .map((num) => String.fromCharCode(num))
+      .join("");
+    output.innerText = translatedText;
+  }
 }
+
+function textToBinaryClickHandler() {
+  // click handler for text to binary button
+  toTextBtn.style.backgroundColor = "#519892";
+  toBinaryBtn.style.backgroundColor = "#428b85";
+  input.value = " ";
+  output.innerText = " ";
+  translateBtn.removeEventListener("click", binaryToText);
+  translateBtn.addEventListener("click", textToBinary);
+}
+
+function binaryToTextClickHandler() {
+  // click handler for binary to text button
+  toTextBtn.style.backgroundColor = "#428b85";
+  toBinaryBtn.style.backgroundColor = "#519892";
+  input.value = " ";
+  output.innerText = " ";
+  translateBtn.removeEventListener("click", textToBinary);
+  translateBtn.addEventListener("click", binaryToText);
+}
+
+textToBinaryClickHandler();
